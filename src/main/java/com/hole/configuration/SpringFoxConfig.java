ed2408385cae3401a6a0a380ff559dff32187590
dlrv <daniel-rocha.silva@gft.com>
2021-12-16 14:54:24 -0300

    refac: Removido imports comentados

--- src/main/java/com/hole/configuration/SpringFoxConfig.java
+++ src/main/java/com/hole/configuration/SpringFoxConfig.java
@@ -17,14 +17,9 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
-// import springfox.documentation.service.AuthorizationScope;
-// import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
-// import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-// import springfox.documentation.swagger.web.SecurityConfiguration;
-// import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 
